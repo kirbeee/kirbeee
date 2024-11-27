d26465b2403a8ae7849b3015d4c87043edf9f941
kirbeee <pspsean1234@gmail.com>
2024-11-27 10:01:31 +0800

    config the setting

--- kirbee-website/docusaurus.config.ts
+++ kirbee-website/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://kirbeee.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -20,6 +20,7 @@ const config: Config = {
   organizationName: 'kirbeee', // Usually your GitHub org/user name.
   projectName: 'kirbeee.github.io', // Usually your repo name.
   trailingSlash: false,
+  deploymentBranch: 'main',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
