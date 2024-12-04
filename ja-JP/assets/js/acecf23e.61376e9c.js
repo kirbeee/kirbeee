e5a37cd4c77003b5faebcdbdacc3dcf683eaadc6
kirbeee <kirbeee@users.noreply.github.com>
2024-12-04 00:28:37 +0000

    deploy: bef9e3a13df77da2d58c866ffb3539dc77594816

---
+++ ja-JP/assets/js/acecf23e.61376e9c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkirbee_website=self.webpackChunkkirbee_website||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/kirbeee/ja-JP/blog","blogTitle":"Blog","authorsListPath":"/kirbeee/ja-JP/blog/authors"}')}}]);
