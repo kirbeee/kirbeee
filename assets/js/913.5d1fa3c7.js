c248662d0ea142357cd72afa800bf62d5517ea1b
kirbeee <kirbeee@users.noreply.github.com>
2025-01-18 15:52:19 +0000

    deploy: 8fffe07f87d47cc4c4a4c63c30a05a7508b6fbf1

---
+++ assets/js/913.5d1fa3c7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkirbee_website=self.webpackChunkkirbee_website||[]).push([[913],{8913:(e,s,b)=>{b.r(s)}}]);
---
+++ zh-TW/assets/js/5e95c892.d159bcf1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkirbee_website=self.webpackChunkkirbee_website||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>b});r(6540);var t=r(4164),u=r(5500),a=r(7559),c=r(2831),i=r(1957),n=r(4848);function b(e){return(0,n.jsx)(u.e3,{className:(0,t.A)(a.G.wrapper.docsPages),children:(0,n.jsx)(i.A,{children:(0,c.v)(e.route.routes)})})}}}]);
