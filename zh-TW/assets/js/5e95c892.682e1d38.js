c3c101be5d5b967d29823ae048fa026d878a7900
kirbeee <kirbeee@users.noreply.github.com>
2025-03-13 02:50:35 +0000

    deploy: 2fe5ad905d477f793b4e969716a9febec98e44c2

---
+++ zh-TW/assets/js/5e95c892.682e1d38.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkirbee_website=self.webpackChunkkirbee_website||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>b});r(96540);var t=r(34164),u=r(45500),a=r(17559),c=r(22831),i=r(41957),n=r(74848);function b(e){return(0,n.jsx)(u.e3,{className:(0,t.A)(a.G.wrapper.docsPages),children:(0,n.jsx)(i.A,{children:(0,c.v)(e.route.routes)})})}}}]);
