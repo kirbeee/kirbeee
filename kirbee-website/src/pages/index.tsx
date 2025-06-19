9403c5d52439234ba7c4a8a45b345ddb620106b8
kirbeee <pspsean1234@gmail.com>
2025-06-19 16:51:45 +0800

    revert

--- kirbee-website/src/pages/index.tsx
+++ kirbee-website/src/pages/index.tsx
@@ -11,7 +11,6 @@ import styles from './index.module.css';
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
-        <SpeedInsights>
         <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
                 <Heading as="h1" className="hero__title">
@@ -28,7 +27,6 @@ function HomepageHeader() {
                 </Link>
             </div>
         </header>
-        </SpeedInsights>
     );
 }
 
