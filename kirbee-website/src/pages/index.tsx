e254b976d5548f02c035cd2eda259bc080bb6272
kirbeee <pspsean1234@gmail.com>
2025-06-20 13:45:23 +0800

    update

--- kirbee-website/src/pages/index.tsx
+++ kirbee-website/src/pages/index.tsx
@@ -4,12 +4,11 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-import Translate, {translate} from "@docusaurus/core/lib/client/exports/Translate";
+import Translate from "@docusaurus/Translate";
 import styles from './index.module.css';
 import { JSX } from 'react';
 
 function HomepageHeader() {
-    const {siteConfig} = useDocusaurusContext();
     return (
         <header className={clsx('hero hero--primary', styles.heroBanner)}>
             <div className="container">
